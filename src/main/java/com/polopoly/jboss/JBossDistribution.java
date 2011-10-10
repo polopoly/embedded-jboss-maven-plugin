1e21547d09db172fcba66f35ffa8087fcd798540
Jonas Wiklund <jwiklund@atex.com>
2011-10-10 14:26:24 +0200

    Added default constructor for JBossDistribution. Closes gh-1

--- src/main/java/com/polopoly/jboss/JBossDistribution.java
+++ src/main/java/com/polopoly/jboss/JBossDistribution.java
@@ -8,6 +8,9 @@ public class JBossDistribution extends ArtifactData {
     public static final JBossDistribution DEFAULT_JBOSS_DISTRIUTION =
             new JBossDistribution("com.polopoly.jboss", "slim-jboss-4.0.5", "1.1", "zip");
 
+    public JBossDistribution() {
+    }
+
     public JBossDistribution(String groupId, String artifactId, String version, String type) {
         this.artifactId = artifactId;
         this.version = version;
