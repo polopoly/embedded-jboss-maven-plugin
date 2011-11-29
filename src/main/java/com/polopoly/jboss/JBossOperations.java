4b0bec88a6b9e19719a63597f5bafdae26abd4cb
Andreas Nilsson <andreas.nilsson@atex.com>
2011-11-29 16:35:28 +0100

    A lot of debug information. /Andreas

--- src/main/java/com/polopoly/jboss/JBossOperations.java
+++ src/main/java/com/polopoly/jboss/JBossOperations.java
@@ -112,7 +112,7 @@ public class JBossOperations {
     // -----------------------------------------------------------
     // MBean manipulators
     // -----------------------------------------------------------
-    private Object getAttribute(String name, String attribute) {
+    public Object getAttribute(String name, String attribute) {
         return getAttribute(objectName(name), attribute);
     }
 
@@ -126,11 +126,11 @@ public class JBossOperations {
         }
     }
 
-    private Object invoke(String name, String operation) throws MojoExecutionException {
+    public Object invoke(String name, String operation) throws MojoExecutionException {
         return invoke(objectName(name), operation, null, null);
     }
 
-    private Object invoke(String name, String operation, Object value) throws MojoExecutionException {
+    public Object invoke(String name, String operation, Object value) throws MojoExecutionException {
         return invoke(objectName(name), operation, new Object[]{value}, new String[]{value.getClass().getName()});
     }
 
