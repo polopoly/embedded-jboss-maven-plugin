250c0a9b350148312bd6567aa06ce68d1fd8ccbb
Andreas Nilsson <andreas.nilsson@atex.com>
2011-12-02 09:04:24 +0100

    #13465 JNP auto discovery should be turned off. /Andreas

--- src/main/java/com/polopoly/jboss/AbstractJBossMBeanMojo.java
+++ src/main/java/com/polopoly/jboss/AbstractJBossMBeanMojo.java
@@ -1,22 +1,25 @@
 package com.polopoly.jboss;
 
-import org.apache.maven.plugin.MojoExecutionException;
-import org.jboss.security.SecurityAssociation;
-import org.jboss.security.SimplePrincipal;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Properties;
 
 import javax.management.MBeanServerConnection;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Properties;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.jboss.security.SecurityAssociation;
+import org.jboss.security.SimplePrincipal;
+import org.jnp.interfaces.NamingContext;
 
 /**
  * Created by bitter on 2011-10-07
  */
-public abstract class AbstractJBossMBeanMojo extends AbstractJBossMojo {
-
+public abstract class AbstractJBossMBeanMojo
+    extends AbstractJBossMojo
+{
     /**
      * Maximum number of retries to get JBoss JMX MBean connection.
      *
@@ -40,9 +43,9 @@ public abstract class AbstractJBossMBeanMojo extends AbstractJBossMojo {
 
     private volatile MBeanServerConnection _connection;
 
-
-    public MBeanServerConnection connect() throws MojoExecutionException {
-
+    public MBeanServerConnection connect()
+        throws MojoExecutionException
+    {
         if (_connection != null) {
             return _connection;
         }
@@ -53,26 +56,26 @@ public abstract class AbstractJBossMBeanMojo extends AbstractJBossMojo {
         // Try to get JBoss jmx MBean connection
         MBeanServerConnection server = null;
         NamingException ne = null;
-        for ( int i = 0; i < retry; ++i )
-        {
-            try
-            {
+
+        for (int i = 0; i < retry; ++i) {
+            try {
                 server = (MBeanServerConnection) ctx.lookup( "jmx/invoker/RMIAdaptor" );
                 break;
             }
-            catch ( NamingException e )
-            {
+            catch (NamingException e) {
                 ne = e;
                 info("Waiting to retrieve JBoss JMX MBean connection... ");
             }
+
             sleep("Thread interrupted while waiting for MBean connection");
         }
 
-        if ( server == null )
-        {
+        if (server == null) {
             throw new MojoExecutionException( "Unable to get JBoss JMX MBean connection: " + ne.getMessage(), ne );
         }
+
         _connection = server;
+
         return server;
     }
 
@@ -80,12 +83,14 @@ public abstract class AbstractJBossMBeanMojo extends AbstractJBossMojo {
      * Determine whether <code>namingPort</code> is free.
      * @return
      */
-    protected boolean isNamingPortFree() {
+    protected boolean isNamingPortFree()
+    {
         try {
             new Socket("127.0.0.1", new Integer(namingPort));
         } catch (IOException e) {
             return false;
         }
+
         return true;
     }
 
@@ -99,33 +104,31 @@ public abstract class AbstractJBossMBeanMojo extends AbstractJBossMojo {
         throws MojoExecutionException
     {
         Properties env = new Properties();
-        env.put( Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory" );
-        env.put( Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces" );
-        env.put( Context.PROVIDER_URL, "127.0.0.1:" + namingPort );
+
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+        env.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
+        env.put(Context.PROVIDER_URL, "127.0.0.1:" + namingPort);
+        env.put(NamingContext.JNP_DISABLE_DISCOVERY, "true");
 
         String username = getUsername();
-        if ( username != null )
-        {
+
+        if (username != null) {
             SecurityAssociation.setPrincipal(new SimplePrincipal(username));
             SecurityAssociation.setCredential(getPassword());
         }
 
-        try
-        {
+        try {
             return new InitialContext( env );
-        }
-        catch ( NamingException e )
-        {
-            throw new MojoExecutionException( "Unable to instantiate naming context: " + e.getMessage(), e );
+        } catch (NamingException e) {
+            throw new MojoExecutionException("Unable to instantiate naming context: " + e.getMessage(), e);
         }
     }
 
-    protected void sleep(String interruptedMessage) {
+    protected void sleep(String interruptedMessage)
+    {
         try {
             Thread.sleep(retryWait * 1000);
-        }
-        catch ( InterruptedException e )
-        {
+        } catch (InterruptedException e) {
             warn(interruptedMessage + "\n" + e.getMessage());
         }
     }
